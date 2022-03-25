c16a1c40665c867db2d7a80105144d396a997e77
Bill Sacks <sacks@ucar.edu>
2022-03-24 22:14:28 -0600

    Use gcc rather than clang for the serial C compiler
    I don't regularly use the serial C compiler for system builds, though I
    do use it for the unit test builds. It seems good to make this change so
    that (I think) I'm using the same compiler for both mpi and serial
    builds (since I think that mpicc currently points to gcc, not clang).
    
    I confirmed that the CTSM unit test build still works with this change.
    I'm having independent problems with the CIME / CESM_share unit test
    build, so I can't test this. An mpi-serial system test builds
    successfully with this change, though fails at the beginning of the run
    with:
    
     *** The MPI_Comm_f2c() function was called before MPI_INIT was invoked.
     *** This is disallowed by the MPI standard.
     *** Your MPI job will now abort.
    
    (I haven't tried an mpi-serial build with clang to see if I run into the
    same problem with clang.)

--- gnu_bishorn.cmake
+++ gnu_bishorn.cmake
@@ -13,8 +13,8 @@ endif()
 # The following paths aren't necessary on my machine because I have my PATH set so that
 # the right compilers are picked up by default. But it doesn't hurt to be explicit.
 set(SFC "/usr/local/bin/gfortran")
-set(SCC "/usr/bin/clang")
-set(SCXX "/usr/bin/clang++")
+set(SCC "/usr/local/bin/gcc")
+set(SCXX "/usr/local/bin/g++")
 set(MPIFC "/usr/local/bin/mpif90")
 set(MPICC "/usr/local/bin/mpicc")
 set(MPICXX "/usr/local/bin/mpic++")
